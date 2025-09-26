ef2766dad7613000e8038f0c80866bf223d72da6
VictorKristiansson <Victor.Kristiansson@stud.sundsgarden.se>
2025-09-26 10:29:30 +0700

    Update server.ts
    completed the api fetch from https://randomuser.me/api.

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,20 @@
+import { count } from "console";
 import express from "express";
 import { z } from "zod";
 
 const app = express();
 const PORT = 3000;
 app.use(express.json());
+
+const userSchema = z.object({
+  name: z.string().min(3).max(12),
+  age: z.number().min(18).max(100).default(28).optional(),
+  email: z.string().email().toLowerCase(),
+});
+
+type User = z.infer<typeof userSchema>;
+
+let users: User[] = [];
 
 app.get("/random-user", async (req, res) => {
   try {
@@ -13,7 +24,10 @@ app.get("/random-user", async (req, res) => {
     }
     const data = await respone.json();
     res.json(data);
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+    console.error(error);
+  }
 });
 
 app.listen(PORT, () => {
